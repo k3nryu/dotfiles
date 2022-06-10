efd0fcb75453d1380cc23467615333c4aa86f6d1
k3nryu <tinkenryu@gmail.com>
2022-06-10 11:46:47 +0900

    update

--- README.md
+++ README.md
@@ -7,25 +7,24 @@
 - Powerlevel10k(zsh theme)
 - zsh-syntax-highlighting
 - zsh-autosuggestions
-- tmux
-- alacritty
 
 ## Installation
-1. Clone repositories to local.
+#### Step1: Clone repositories to local.
 ```bash
 git clone --recursive -b Linux https://github.com/k3nryu/dotfiles.git
 ```
-2. Execute 'install.sh' shell script.
+#### Step2: Execute 'install.sh' shell script.
 ```bash
 docfiles/install.sh
 ```
 
-## zsh
+## Update
+```bash
 
-### oh-my-zsh (zsh configuration manager)
-https://github.com/ohmyzsh/ohmyzsh
+```
 
-### Powerlevel10k (zsh theme)
-https://github.com/romkatv/powerlevel10k
-
-## tmux
+## Uninstall
+```bash
+rm -rf ~/dotfiles
+rm -rf ~/.oh-my-zsh
+```
--- install.sh
+++ install.sh
@@ -4,25 +4,22 @@
 zsh_bins=`grep 'zsh' /etc/shells | wc -l`
 if [ $zsh_bins == 0 ];
 then
-	dnf -y install zsh
+	sudo dnf -y install zsh
 fi
+sudo dnf -y install util-linux-user
 
-
+## Install zsh plugins
+### p10k theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+### systax highlight
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+### auto suggest
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-#git clone --recursive -b Linux git@github.com:k3nryu/dotfiles.git
-#git clone --recursive -b Linux https://github.com/k3nryu/dotfiles.git
 
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.oh-my-zsh ~/.oh-my-zsh
 ln -sf ~/dotfiles/.p10k.zsh ~/.p10k.zsh
 
-sudo dnf -y install util-linux-user
 chsh -s $(which zsh)
-echo $SHELL
-
 exec zsh
-
-
+echo $SHELL
